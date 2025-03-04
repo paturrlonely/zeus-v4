a03dccbb9c28f6714770e5fcaf2b314a2ece796f
paturrlonely <boboiapi54321@gmail.com>
2025-03-05 06:53:48 +0700

    Add files via upload

---
+++ storage/config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [7530314274], // ganti jadi id mu
+    botToken: "7574099627:AAF4-Gyl-7-qusHy0KJoUd4h9W3WW1vp-uI", //isi make token bot mu
+}
+
+module.exports = global;
